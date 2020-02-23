2460eb7882dc691b96864ab0c955534a2cfdd4ca
Vgeorgee <cracken1111@gmail.com>
2020-02-23 16:18:58 +0100

    less test request

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -8,13 +8,13 @@ reqs = []
 
 ;(async function main() {
 
-    for(let i = 0; i < 1000; i++){
+    for(let i = 0; i < 10; i++){
         reqs.push(await request.get('/simple'))
     }
     
     //Promise.all(reqs)
     
-    for(let i = 0; i < 1000; i++){
+    for(let i = 0; i < 10; i++){
         console.log(reqs[i].text)
     }
     
