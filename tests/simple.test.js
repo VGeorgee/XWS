4b162bd6749caa7dd9db0956c77860699eb37eea
Vgeorgee <cracken1111@gmail.com>
2020-03-29 11:43:55 +0200

    code quality

--- tests/simple.test.js
+++ tests/simple.test.js
@@ -2,27 +2,20 @@ let request = require('supertest')
 const DEFAULT_PORT = 28015
 request = request('http://localhost:' + (DEFAULT_PORT || 80))
 
-reqs = []
-
 
 
 ;(async function main() {
 
     for(let i = 0; i < 10; i++){
-        reqs.push(await request.get('/simple'))
+        await request.get('/simple')
     }
-    
-    //Promise.all(reqs)
-    
-    for(let i = 0; i < 10; i++){
-        console.log(reqs[i].text)
-    }
+
     await request.get('/')
     await request.get('/demofunction')
     await request.get('/favicon.ico')
     try {
         await request.get('/exit')
     } catch (error) {
-        
+
     }
 })()
